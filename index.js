2d0d56291517e4741a9040bd8adbd4909986c118
Esteban <estebanpablo89@gmail.com>
2022-12-08 09:30:46 -0500

    Add addDays

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict'
 
+const _1day = 24 * 60 * 60 * 1000
+
 const toUTCDateString = d =>
   `${d.getUTCFullYear()}-${('0' + (d.getUTCMonth() + 1)).slice(-2)}-${('0' + d.getUTCDate()).slice(-2)}`
 
 const isValidDate = d => !Number.isNaN(d.getTime())
 
+const addDays = (date, days) => date.getTime() + days * _1day
+
 module.exports = {
   toUTCDateString,
-  isValidDate
+  isValidDate,
+  addDays
 }
